39ea28a5d38c1519092ce10b012488c0f63c69af
Juan Pablo Schiaffino <jpschiaffino@gmail.com>
2022-02-20 00:28:22 -0300

    config for alacritty and fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,16 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+# if status is-interactive
+#     # Commands to run in interactive sessions can go here
+# end
+set fish_greeting ""
 
+# Aliases
+
+alias grep "grep --color=auto"
+alias cat "bat --style=plain --paging=never"
+alias ls "exa --group-directories-first"
+alias tree "exa -T"
+alias dotfiles "git --git-dir $HOME/.dotfiles/ --work-tree $HOME"
+
+# Prompt
+
+starship init fish | source
