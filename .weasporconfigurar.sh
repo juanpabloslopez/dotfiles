27bddeb0099e639c1a29584335a46539e4953264
Juan Pablo Schiaffino <jpschiaffino@gmail.com>
2022-03-03 12:16:28 -0300

    correct arrows for mode vim in prompt fish

--- .weasporconfigurar.sh
+++
@@ -1,9 +0,0 @@
-(V) -> ready
-(F) -> not ready
-1) Prompt of fish (alejandroherrera@mbam1~/dir (main) >) # with icons (V)
-2) The symbol "~"  (fish) (F)
-3) Shortcuts for Amethyst (F)
-
-3/3/22 01:34am
-1) Colors prompt of fish
-2) [I] Prompt of fish
